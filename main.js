4ce491199d6932ac25bbf7c93d00603baf445b35
cluracan1 <cluracan1@gmail.com>
2024-09-07 09:40:31 +0100

    set background to change on time of day (currently just in main)

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 import "./style.css";
-import backgroundUrl from "./blue-sky.jpg";
+import morningBackgroundUrl from "./blue-morning.jpg";
+import afternoonBackgroundUrl from "./blue-afternoon.jpg";
+import eveningBackgroundUrl from "./blue-night.jpeg/";
 import { format } from "date-fns";
 import { Today } from "./today";
 import { TaskHolder } from "./taskHolder";
 import { Task } from "./task";
 const backgroundHolder = document.getElementById("background");
-backgroundHolder.style.backgroundImage = `url("${backgroundUrl}")`;
+
+let currentHour = new Date().getHours();
+if (currentHour < 12) {
+  backgroundHolder.style.backgroundImage = `url("${morningBackgroundUrl}")`;
+} else if (currentHour < 18) {
+  backgroundHolder.style.backgroundImage = `url("${afternoonBackgroundUrl}")`;
+} else {
+  backgroundHolder.style.backgroundImage = `url("${eveningBackgroundUrl}")`;
+}
 
 const contentHolder = document.getElementById("content");
 
 console.log(format(new Date(), "a 'in the' eeee"));
 
-// localStorage.setItem(
-//   "Lara-toDoList-tasks",
-//   JSON.stringify([
-//     new Task("To do Item 1", 1, format(new Date(), "yyyy-MM-dd"), "", false),
-//     new Task(
-//       "To do item 2",
-//       2,
-//       format(new Date(), "yyyy-MM-dd"),
-//       "some notes",
-//       false,
-//       "gardening",
-//       ["priority"]
-//     ),
-//     new Task("To do item 3", 3, format(new Date(), "yyyy-MM-dd"), false),
-//   ])
-// );
+localStorage.setItem(
+  "Lara-toDoList-tasks",
+  JSON.stringify([
+    new Task(
+      "To do Item 1",
+      1,
+      format(new Date(), "yyyy-MM-dd"),
+      "",
+      false,
+      "fishing",
+      ["priority"]
+    ),
+    new Task(
+      "To do item 2",
+      2,
+      format(new Date(), "yyyy-MM-dd"),
+      "some notes",
+      false,
+      "gardening",
+      ["priority", "family"]
+    ),
+    new Task("To do item 3", 3, format(new Date(), "yyyy-MM-dd"), false),
+  ])
+);
 
 localStorage.setItem("Lara-toDoList-currentID", 3);
 
